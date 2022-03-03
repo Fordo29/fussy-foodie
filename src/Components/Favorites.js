f819fa59582d5d72b4bf54643dded235035ab583
Christine Rowland <fordo29@gmail.com>
2022-03-03 15:41:05 -0700

    Add Favorite Component build

---
+++ src/Components/Favorites.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Recipes from "./Recipes";
+import '../Styling/Favorites.scss';
+
+const Favorites = (props) => {
+
+  return (
+    <section>
+      {!props.favorites.length && <h2 className="empty-favorites-msg">You haven't added any favorites yet!</h2>}
+     <Recipes recipes={props.favorites}/> 
+    </section>
+     
+  )
+}
+
+export default Favorites;

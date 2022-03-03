178448b3a0b07a6d869a5f51207d0228daa80cb6
Christine Rowland <fordo29@gmail.com>
2022-03-03 15:40:27 -0700

    Add Favorite functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,9 @@ import { Route, Switch } from 'react-router-dom'
 import NavBar from './NavBar';
 import Recipes from './Recipes';
 import getData from '../apiCalls';
+import SingleRecipe from './SingleRecipe';
+import Favorites from './Favorites'
+import ErrorPage from './ErrorPage'
 
 import '../Styling/App.scss';
 
@@ -11,14 +14,14 @@ class App extends Component {
     super()
     this.state = {
       recipes: [],
-      isSingleRecipt: false,
-      recipe: '',
+      isSingleRecipe: false,
+      favorites: [],
       error: ''
     }
   }
 
   componentDidMount = () => {
-    getData()
+    getData('foods')
       .then(recipesData => {
         this.setState({ recipes: recipesData.foods})
       })
@@ -29,14 +32,32 @@ class App extends Component {
     this.setState({ isSingleRecipe: false })
   }
 
+  addFavorite = (newRecipe) => {
+    const findRecipe = this.state.recipes.find(recipe => recipe.id === newRecipe.id)
+    if(!this.state.favorites.includes(findRecipe)){
+      this.setState({favorites: [...this.state.favorites, findRecipe]  });
+    } else {
+      this.setState({...this.state})
+    }
+  }
+
+  removeFavorite = (id) => {
+    const unfav = this.state.recipes.find(recipe => recipe.id === id)
+    const filteredFavs = this.state.favorites.filter(fav => fav.id !== unfav.id);
+    this.setState({ favorites: filteredFavs });
+  }
+
 
   render() {
-
+    // console.log(this.state.favorites)
     return (
       <main className="main-container">
         <NavBar />
         <Switch>
           <Route exact path='/' render={() => <Recipes recipes={this.state.recipes}  />}/>
+          <Route exact path='/:id/selectedRecipe' render={({ match }) => <SingleRecipe id={match.params.id} addFavorite={this.addFavorite} removeFavorite={this.removeFavorite} isFavRecipe={this.state.isFavRecipe}/>}/>
+          <Route exact path='/favorites' render={() => <Favorites favorites={this.state.favorites}/>}/>
+          <Route render={() => <ErrorPage />}/>
         </Switch>
       </main>
     )

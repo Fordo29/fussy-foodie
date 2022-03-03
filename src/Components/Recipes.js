fba640c0bbf61197a065cf626617a69947df16a3
Christine Rowland <fordo29@gmail.com>
2022-03-02 17:11:44 -0700

    Add recipe container component

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -2,20 +2,21 @@ import React from "react";
 import RecipeCard from "./RecipeCard";
 import '../Styling/Recipes.scss'
 
-const Recipes = ({ allRecipes }) => {
-  const recipeCards = allRecipes.map((recipe) => {
+const Recipes = ({ recipes }) => {
+  const recipeCards = recipes.map(recipe => {
+    console.log(recipe)
     return (
       <RecipeCard
         key={recipe.id}
         id={recipe.id}
-        title={recipe.title}
+        title={recipe.recipeTitle}
         recipeImage={recipe.image}
       />
     )
   })
   return (
-    <section>
-      <h1>recipe container</h1>
+    <section className="recipes-container">
+      {recipeCards}
     </section>
   )
 }

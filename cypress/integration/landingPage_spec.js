07c85500960dcb3c8008ae7f9bb863f027a68a37
Christine Rowland <fordo29@gmail.com>
2022-03-05 10:51:26 -0700

    Add testing for the button clicks on the landing page

--- cypress/integration/landingPage_spec.js
+++ cypress/integration/landingPage_spec.js
@@ -1,4 +1,4 @@
-describe('Feedback Loop login flows', () => {
+describe('Landing Page user flow', () => {
   beforeEach(() => {
     cy.intercept('https://picky-eaters.herokuapp.com/api/v1/foods', { fixture: 'recipes.json' })
     cy.visit('http://localhost:3000');
@@ -21,6 +21,19 @@ describe('Feedback Loop login flows', () => {
   it('Should display 5 recipe cards', () => {
     cy.get('.recipe-card')
       .should('have.length', 5)
+  });
+
+  it('Should go to the single recipe page when you click the Try It! button', () => {
+    cy.get('.recipe-detail-link').contains('Try It!').click()
+      .url().should('eq', 'http://localhost:3000/1/selectedRecipe')
+  });
+
+  it('Should go to the single recipe page when you click the Try It! button', () => {
+    cy.get('.favorite-button').contains('Add to Favorites').click()
+    cy.get('.nav-bar').find('.favorites-link').click()
+      .url().should('eq', 'http://localhost:3000/favorites')
+    cy.get('.recipe-card')
+      .should('have.length', 1)
   });
 
 });

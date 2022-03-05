d84b696fa11bbb42659fc329677446ff52b55943
Christine Rowland <fordo29@gmail.com>
2022-03-05 12:10:20 -0700

    Edit error message

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -5,8 +5,8 @@ import '../styling/ErrorPage.scss'
 const ErrorPage = () => {
   return (
     <section className="message-container">
-      <h2>Page not found</h2>
-      <p>So sorry, that URL does not exist</p>
+      <h2 className='error-message'>Page not found. So sorry, that URL does not exist</h2>
+  
     </section>
   )
 }

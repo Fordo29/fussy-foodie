17ebb455030efbe540c8b82b589e95fa4ab3ce99
Christine Rowland <fordo29@gmail.com>
2022-03-04 17:03:19 -0700

    Add variables to easily change colors moving forward

--- src/Components/FavoriteCard.js
+++ src/Components/FavoriteCard.js
@@ -6,7 +6,7 @@ const FavoriteCard = ({ recipeImage, title, id, removeFavorite}) => {
   return (
     <section className="recipe-card">
       <img className="recipe-cover" src={recipeImage} alt={`${title} cover`}/>
-        <h2 className="book-title">{title}</h2>
+        <h2 className="recipe-title">{title}</h2>
       <Link key={id} to={`/${id}/selectedRecipe`} className="recipe-detail-link">Try It</Link>
       <button className="unfavorite-button" onClick={() => removeFavorite(id)}>Remove from Favorites</button>
     </section>

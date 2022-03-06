efd9d57c221d4349300532c97c48f0cbddf9ebf1
Christine Rowland <fordo29@gmail.com>
2022-03-05 23:38:00 -0700

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styling/Footer.scss'
+
+const Footer = () => (
+  <div className="footer">
+    <p>This is some content in sticky footer</p>
+  </div>
+);
+
+export default Footer;

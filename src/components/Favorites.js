2de5e5b5f43d7faed569c9201a4d4501cbc60be1
Christine Rowland <fordo29@gmail.com>
2022-03-05 17:00:09 -0700

    Add prop types to Favorites component

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import FavoritesContainer from "./FavoritesContainer";
 import '../styling/Favorites.scss';
+import PropTypes from 'prop-types';
 
-const Favorites = (props) => {
+const Favorites = ({favorites, removeFavorite}) => {
 
   return (
     <section>
-      {!props.favorites.length && <h2 className="empty-favorites-msg">You haven't added any favorites yet! Go find some favorites!</h2>}
-     <FavoritesContainer favorites={props.favorites} removeFavorite={props.removeFavorite}/> 
+      {!favorites.length && <h2 className="empty-favorites-msg">You haven't added any favorites yet! Go find some favorites!</h2>}
+     <FavoritesContainer favorites={favorites} removeFavorite={removeFavorite}/> 
     </section>
      
   )
 }
 
 export default Favorites;
+
+Favorites.propTypes = {
+  favorites: PropTypes.array,
+  removeFavorite: PropTypes.func
+
+}

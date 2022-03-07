7d631cb233764008eccbbbc1d83255736fb72c94
Christine Rowland <fordo29@gmail.com>
2022-03-06 18:55:17 -0700

    Remove unused variable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../styling/Footer.scss'
 import { Link } from 'react-router-dom'
-import FooterDetails from "./FooterDetails";
 
 const Footer = () => {
   return (
